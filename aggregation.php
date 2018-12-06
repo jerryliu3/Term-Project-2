dfe7a6670b5cb9672697abe435774eee8c64ecd8
Jerry Liu <3jerryliu@gmail.com>
2018-12-06 16:28:13 +0900

    Implement join, subquery and aggregation usage

--- aggregation.php
+++ aggregation.php
@@ -1,5 +1,4 @@
 <?php
-$table = filter_input(INPUT_POST, 'table');
 $date = filter_input(INPUT_POST, 'date');
 
 $servername = "localhost";
@@ -18,17 +17,21 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT cuname, order_number, waname, fee, feedback, ddate FROM customer
-INNER JOIN dining_table ON customer.table_number=dining_table.tnumber
+$sql = "SELECT SUM(fee), waname FROM waitor
+LEFT JOIN dining_table ON waitor.waID=dining_table.waitorID
+FULL OUTER JOIN orderlist ON dining_table.order_number=orderlist.onumber
+WHERE (orderlist.ddate='$date')
+GROUP BY waitor.waname";
+$sql = "SELECT SUM(fee), waname FROM dining_table
 INNER JOIN orderlist ON dining_table.order_number=orderlist.onumber
-LEFT JOIN waitor ON dining_table.waitorID=waitor.waID
-WHERE (customer.table_number='$table')
-AND (orderlist.ddate='$date')";
+RIGHT JOIN waitor ON dining_table.waitorID=waitor.waID
+WHERE (orderlist.ddate='$date')
+GROUP BY waname";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-	echo "Information for table $table on $date". '</td>';
-echo "<table><tr><th>Customer Name | </th><th>Order Number | </th><th>Waitor Name | </th><th>Fee | </th><th>Feedback | </th><th>Date | </th></tr>";
+	echo "Performance of waitors on $date". '</td>';
+	echo "<table><tr><th>Customer Name | </th><th>Order Number | </th><th>Waitor Name | </th><th>Fee | </th><th>Feedback | </th><th>Date | </th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
 		echo"<tr>";
